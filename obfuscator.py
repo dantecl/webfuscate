54b50bbb45a703fb34d4efa806b3fd6adf861771
Some Dude <somedude@yournearestbar.com>
2017-03-31 01:46:38 -0700

    working downloader

--- obfuscator.py
+++ obfuscator.py
@@ -2,19 +2,40 @@
 
 import urllib2
 import zipfile
+import csv
+import os
+import fnmatch
 
 sitelist = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
-scriptdir = "/opt/webfuscate"
+scriptdir = "/tmp/webfuscate/"
 
 
 def unziplist(srcfile):
+    """
+    Unzip the list to $scriptdir
+    """
     with zipfile.ZipFile(srcfile, "r") as zip_ref:
-        zip_ref.extractall("targetdir")
+        zip_ref.extractall(scriptdir)
     return
 
 
+def find(pattern, path):
+    """
+    Find a file based on a pattern
+    """
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+
 def downloadlist(ziplink):
-    file_name = ziplink.split('/')[-1]
+    """
+    Download our site list
+    """
+    file_name = scriptdir + ziplink.split('/')[-1]
     u = urllib2.urlopen(ziplink)
     f = open(file_name, 'wb')
     meta = u.info()
@@ -23,16 +44,23 @@ def downloadlist(ziplink):
     fsize_dl = 0
     block_sz = 8192
     while True:
-        bfr = u.read(block_sz)
-        if not bfr:
+        buffer = u.read(block_sz)
+        if not buffer:
             break
-
-    fsize_dl += len(buffer)
-    f.write(buffer)
+        fsize_dl += len(buffer)
+        f.write(buffer)
     status = r"%10d  [%3.2f%%]" % (fsize_dl, fsize_dl * 100. / file_size)
     status = status + chr(8)*(len(status)+1)
     print status,
     f.close()
+    return file_name
+
 
 # download our list to the script's folder
-downloadlist(sitelist)
+if not os.path.isdir(scriptdir):
+    os.mkdir(scriptdir)
+
+dnld = downloadlist(sitelist)
+unziplist(dnld)
+csvfile = find('*.csv', scriptdir)
+full_list = csv.reader(open(csvfile[0]), delimiter=',')
