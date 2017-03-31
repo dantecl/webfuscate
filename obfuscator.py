e5e721a47b447d3c646ca9d092d193ed5044f73c
Some Dude <somedude@yournearestbar.com>
2017-03-31 00:05:13 -0700

    update

--- obfuscator.py
+++ obfuscator.py
@@ -4,36 +4,35 @@ import urllib2
 import zipfile
 
 sitelist = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
-scriptdir = AA
+scriptdir = "/opt/webfuscate"
 
-def unziplist {
-  with zipfile.ZipFile("file.zip","r") as zip_ref:
-     zip_ref.extractall("targetdir")
-}
 
-def downloadlist {
-file_name = sitelist.split('/')[-1]
-u = urllib2.urlopen(sitelist)
-f = open(file_name, 'wb')
-meta = u.info()
-file_size = int(meta.getheaders("Content-Length")[0])
-print "Downloading: %s Bytes: %s" % (file_name, file_size)
+def unziplist(srcfile):
+    with zipfile.ZipFile(srcfile, "r") as zip_ref:
+        zip_ref.extractall("targetdir")
+    return
 
-file_size_dl = 0
-block_sz = 8192
-while True:
-    buffer = u.read(block_sz)
-    if not buffer:
-        break
 
-    file_size_dl += len(buffer)
+def downloadlist(ziplink):
+    file_name = ziplink.split('/')[-1]
+    u = urllib2.urlopen(ziplink)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+    fsize_dl = 0
+    block_sz = 8192
+    while True:
+        bfr = u.read(block_sz)
+        if not bfr:
+            break
+
+    fsize_dl += len(buffer)
     f.write(buffer)
-    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = r"%10d  [%3.2f%%]" % (fsize_dl, fsize_dl * 100. / file_size)
     status = status + chr(8)*(len(status)+1)
     print status,
+    f.close()
 
-f.close()
-}
-
-#download our list to the script's folder
-
+# download our list to the script's folder
+downloadlist(sitelist)
