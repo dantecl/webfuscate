32f97f3d822aa02e8d20bb34026d421d86ca58e0
Some Dude <somedude@yournearestbar.com>
2019-02-23 13:25:49 -0800

    Working version without looping

--- obfuscator.py
+++ obfuscator.py
@@ -5,6 +5,9 @@ import zipfile
 import csv
 import os
 import fnmatch
+from selenium import webdriver
+import time
+import random
 
 sitelist = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
 scriptdir = "/tmp/webfuscate/"
@@ -40,7 +43,7 @@ def downloadlist(ziplink):
     f = open(file_name, 'wb')
     meta = u.info()
     file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+    print("Downloading: %s Bytes: %s") % (file_name, file_size)
     fsize_dl = 0
     block_sz = 8192
     while True:
@@ -51,9 +54,35 @@ def downloadlist(ziplink):
         f.write(buffer)
     status = r"%10d  [%3.2f%%]" % (fsize_dl, fsize_dl * 100. / file_size)
     status = status + chr(8)*(len(status)+1)
-    print status,
+    print(status)
     f.close()
     return file_name
+
+
+def browselink(link):
+    """
+    Browse the provided link and click something at random inside
+    :param link: a web link
+    :return:
+    """
+    #driver = webdriver.PhantomJS()
+    #driver.set_window_size(1120, 550)
+    #driver.get(link)
+    #driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+    #driver.find_element_by_id("search_button_homepage").click()
+    #print(driver.current_url)
+    #driver.quit()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(link)
+    links = driver.find_elements_by_partial_link_text('')
+    l = links[random.randint(0, len(links)-1)]
+    l.click()
+    print(driver.current_url)
 
 
 # download our list to the script's folder
@@ -64,3 +93,6 @@ dnld = downloadlist(sitelist)
 unziplist(dnld)
 csvfile = find('*.csv', scriptdir)
 full_list = csv.reader(open(csvfile[0]), delimiter=',')
+chosen_url = random.choice(list(full_list))
+print(chosen_url)
+browselink("http://" + chosen_url[1])
